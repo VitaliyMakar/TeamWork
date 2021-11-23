7a35037df202a2074e8d386d921eb5ea29088dcb
Anzhela Schults <59295431+anzhelaschults@users.noreply.github.com>
2021-11-23 15:34:48 +0100

    Update cartItem.js
    import moment from "moment-timezone"
    export formatDate function

--- cart-product/src/components/cartItem.js
+++ cart-product/src/components/cartItem.js
@@ -1,18 +1,21 @@
 import * as React from 'react';
 import {useSelector, useDispatch} from "react-redux";
 import {setActiveCart} from "../store/actionsCreator";
-
+import moment from "moment-timezone"
 
 export const CartItem = ({cart}) => {
 
     const user = useSelector(state => state.users.find(user => user.id === cart.userId))
     const dispatch = useDispatch()
-    const dateString = (string) => {
-        const d = new Date(string)
-        return ("0" + d.getDate()).slice(-2)
-            + '.' + ("0" + (d.getMonth() + 1)).slice(-2)
-            + '.' + d.getFullYear()
+    function formatDate(date) {
+        return moment(date).format("DD.MM.YYYY")
     }
+    // const dateString = (string) => {
+    //     const d = new Date(string)
+    //     return ("0" + d.getDate()).slice(-2)
+    //         + '.' + ("0" + (d.getMonth() + 1)).slice(-2)
+    //         + '.' + d.getFullYear()
+    // }
 
     const selectActive = () => {
         dispatch(setActiveCart(cart))
@@ -20,7 +23,8 @@ export const CartItem = ({cart}) => {
 
     return !user ? (<div>another Wait</div>) : (
         <div onClick={selectActive}>
-            <p>{user.name.firstname} {user.name.lastname} / Ordered on: {dateString(cart.date)}</p>
+            {/*<p>{user.name.firstname} {user.name.lastname} / Ordered on: {dateString(cart.date)}</p>*/}
+            <p>{user.name.firstname} {user.name.lastname} / Ordered on: {formatDate(cart.date)}</p>
         </div>
     );
 };
